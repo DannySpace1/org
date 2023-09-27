5b2b2ec639568855984d090d941baa1bef60f269
Daniel López <141090206+DannySpace1@users.noreply.github.com>
2023-09-27 10:37:33 -0600

    a

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,11 +1,11 @@
 import "./footer.css"
 
 const Footer = () =>{
-    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
+    return <footer className='footer' style={{ backgroundImage: "url(./img/footer.png)" }}>
     <div className='redes'>
         
         <a href='https://twitter.com/DannySpace01'>
-            <img src="/img/twitter.png" alt='twitter' />
+            <img src="./img/twitter.png" alt='twitter' />
         </a>
         <a href='https://www.instagram.com/danny_space_oficial/'>
             <img src="./img/instagram.png" alt='instagram' />
